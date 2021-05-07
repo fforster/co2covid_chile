375507986387d15a031ec032ff84809187e21279
Francisco Forster <francisco.forster@gmail.com>
2021-05-07 12:20:14 -0400

    Added config for Arduino Uno

---
+++ arduino/SensorArduinoUNO/Sensor_config.h
@@ -0,0 +1,4 @@
+//ID del sensor
+//-----------------------------------------------------------------
+//Formato: Tres Mayúsculas y 2 números. Obs: .equals() es case sensitive
+String StringId = "CPV01"; 
